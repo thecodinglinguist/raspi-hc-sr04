8875817ab2a8ce9d94871adc8afaf467db09dd74
Jonas Eliasson <jonas.eliasson@babyshop.se>
2021-11-16 10:03:36 +0100

    refactor

--- src/raspiSonar.cc
+++ src/raspiSonar.cc
@@ -27,15 +27,16 @@ private:
 public:
   int TriggerPin;
   int EchoPin;
+https://stackoverflow.com/questions/64961662/v8-updating-code-using-deprecated-v8localv8value-v8objectgetv8local
   static NAN_METHOD(New) { 
     v8::Local<v8::Context> context = info.GetIsolate()->GetCurrentContext();
-    Nan::TryCatch try_catch;
     if (info.IsConstructCall()) {
+      v8::Local<v8::Object> parameterObject = info[0]->ToObject(context).ToLocalChecked();
       Nan::MaybeLocal<v8::Value> triggerPinValue = Nan::Get(parameterObject, Nan::New<v8::String>("triggerPin").ToLocalChecked());
       Nan::MaybeLocal<v8::Value> echoPinValue = Nan::Get(parameterObject, Nan::New<v8::String>("echoPin").ToLocalChecked()));
       Nan::MaybeLocal<v8::Value> skipCallToSetup = Nan::Get(parameterObject, Nan::New<v8::String>("callWiringPiSetup").ToLocalChecked());
 
-      int triggerPin = To<uint32_t>(triggerPinValue)).FromMaybe();
+      int triggerPin = To<uint32_t>(triggerPinValue).FromMaybe();
       int echoPin = To<uint32_t>(echoPinValue).FromMaybe();
       bool callWiringPiSetup = To<bool>(skipCallToSetup).FromMaybe(true);
 
