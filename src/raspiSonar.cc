bcdfca21d2a62b75e220c21d2dd804cf781ccfe5
Jonas Eliasson <jonas.eliasson@babyshop.se>
2021-11-16 10:14:58 +0100

    refactor

--- src/raspiSonar.cc
+++ src/raspiSonar.cc
@@ -35,9 +35,9 @@ public:
       Nan::MaybeLocal<v8::Value> echoPinValue = Nan::Get(parameterObject, Nan::New<v8::String>("echoPin").ToLocalChecked());
       Nan::MaybeLocal<v8::Value> skipCallToSetup = Nan::Get(parameterObject, Nan::New<v8::String>("callWiringPiSetup").ToLocalChecked());
 
-      int triggerPin = To<uint32_t>(triggerPinValue.FromMaybe())
-      int echoPin = To<uint32_t>(echoPinValue.FromMaybe())
-      bool callWiringPiSetup = To<bool>(skipCallToSetup.FromMaybe()).FromMaybe(true);
+      int triggerPin = To<uint32_t>(triggerPinValue.ToLocal()).FromMaybe();
+      int echoPin = To<uint32_t>(echoPinValue.ToLocal()).FromMaybe();
+      bool callWiringPiSetup = To<bool>(skipCallToSetup.ToLocal()).FromMaybe(true);
 
       RaspiSonar *obj = new RaspiSonar(triggerPin, echoPin, callWiringPiSetup);
 
